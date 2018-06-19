36b90b23bc638d3dfef916794076fb3d46f0bb60
Vijay Sehgal <vijay.b.sehgal@gmail.com>
2018-06-19 21:10:28 +0530

    Changes to q1 and q3 of Assignment 10

--- Assignment_10/q1.py
+++ Assignment_10/q1.py
@@ -1,14 +1,18 @@
 class Animal:
-   def animal_attributes(self,legs):
-       print("Animals have ", legs, " legs")
+
+   def animal_attributes(self, no_of_legs):
+       print("Animals have ", no_of_legs, " legs")
 
 #Inheriting class Animal
 
 class Tiger(Animal):
+
    def tiger_atrributes(self, nutrition):
-       print("Tiger is ",nutrition)
+       print("Tiger is ", nutrition)
+
 #Creating Objects and calling them
-tig=Tiger()
-tig.animal_attributes(4)
-tig.tiger_atrributes('carnivores')
+
+tiger = Tiger()
+tiger.animal_attributes(4)
+tiger.tiger_atrributes('carnivores')
 
--- Assignment_10/q3.py
+++ Assignment_10/q3.py
@@ -1,21 +1,28 @@
 class Cop:
-   def __init__(self,name,experience,):
-       self.name=name
-       self.experience=experience
-   def display_details(self):
-       print(self.name,"cop is on duty with",self.experience,"years of experience")
-   def update_details(self):
-       self.name='Sherlock'
-       self.experience=7
-class Mission(Cop):
-   def __init(self):
-       super(Mission,self).__init()
-   def display_mission(self,mission):
-       print("The active mission is",mission)
 
-   def assign_cop(self,mission):
-       print(self.name,"has been assigned",mission)
-mission=Mission('Sherlock',4)
+   def __init__(self, name, experience):
+       self.name = name
+       self.experience = experience
+
+   def display_details(self):
+       print(self.name, "cop is on duty with", self.experience, "years of experience")
+
+   def update_details(self):
+       self.name = 'Sherlock'
+       self.experience = 7
+
+class Mission(Cop):
+
+   def __init(self):
+       super(self, mission).__init()
+
+   def display_mission(self, mission):
+       print("The active mission is", mission)
+
+   def assign_cop(self, mission):
+       print(self.name, "has been assigned", mission)
+
+mission = Mission('Sherlock', 4)
 mission.display_mission('Secret')
 mission.display_details()
 mission.assign_cop('Secret')
