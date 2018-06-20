39bbe7ddbfe0287fb4f59a2183510c8dc0216b9b
Vijay Sehgal <vijay.b.sehgal@gmail.com>
2018-06-20 13:12:36 +0530

    Changes to q3 and q4 of assignment 10

--- Assignment_10/q4.py
+++ Assignment_10/q4.py
@@ -1,17 +1,30 @@
 class Shape:
-   def __init__(self,length,breadth):
-       self.length=length
-       self.breadth=breadth
-   def area(self):
-       print("The area is",self.length*self.breadth,"units")
+
+    def __init__(self, length, breadth):
+       self.length = length
+       self.breadth = breadth
+
+    def area(self):
+        result = (self.length * self.breadth)
+        print("The area is: ", result)
 
 class  Rectangle(Shape):
-   def __init__(self):
-       super(Rectangle,self).__init__()
+
+    def __init__(self):
+
+        super(Rectangle, self).__init__()
+
 class Square(Shape):
-   def __init__(self):
-       super(Square,self).__init__()
-rect=Shape(2,4)
+
+    def __init__(self):
+
+        super(Square, self).__init__()
+
+
+rect = Shape(2, 4)
+
 rect.area()
-sq=Shape(4,4)
+
+sq = Shape(4, 4)
+
 sq.area()
